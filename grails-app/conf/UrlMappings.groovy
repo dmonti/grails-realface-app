c56b598366b5b077c7c93a973ace824829955108
Daniel Monti <dms.monti@gmail.com>
2015-01-15 15:42:13 -0200

    Initial commit.

---
+++ grails-app/conf/UrlMappings.groovy
@@ -0,0 +1,15 @@
+class UrlMappings
+{
+	static mappings =
+    {
+        "/$controller/$action?/$id?(.$format)?"{
+            constraints {
+                // apply constraints here
+            }
+        }
+
+//        "/"(controller:"home", action: "index")
+        "/"(view:"/index")
+        "500"(view:'/error')
+	}
+}

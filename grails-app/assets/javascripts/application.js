c6c144354432eb990c2735bca5781f602c0d8205
Daniel Monti <dms.monti@gmail.com>
2015-01-15 21:48:43 -0200

    Main app JS.

--- grails-app/assets/javascripts/application.js
+++ grails-app/assets/javascripts/application.js
@@ -2,12 +2,15 @@
 //
 // Any JavaScript file within this directory can be referenced here using a relative path.
 //
-// You're free to add application-wide JavaScript to this file, but it's generally better 
+// You're free to add application-wide JavaScript to this file, but it's generally better
 // to create separate JavaScript files as needed.
 //
-//= require jquery
-//= require_tree .
+//= require jquery-2.1.3.min
+//= require handlebars-v2.0.0
+//= require ember-1.9.1
+//= require bootstrap.min
 //= require_self
+//= require_tree .
 
 $(function() {
 });

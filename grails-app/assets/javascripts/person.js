bd073c33ea1a509e7ced3e8f3df2fbf2ecc33615
Daniel Monti <dms.monti@gmail.com>
2015-01-17 18:09:13 -0200

    Data load text and submit buttons changed to primary style.

--- grails-app/assets/javascripts/person.js
+++ grails-app/assets/javascripts/person.js
@@ -8,26 +8,28 @@ $(function() {
 
 function PersonForm($form) {
     var self = this;
-    var $btnSubmit = $form.find("button[type=submit]");
+    var $btn = $form.find("button[type=submit]");
 
     var submit = function(e) {
-        $btnSubmit.attr("disabled",  "disabled");
         try {
-            var url = $form.attr("action");
-            var data = $form.serialize();
-            $.post(url, data, function(result) {
-                if (result.status)
-                    toastr.success(result.message)
-                else
-                    toastr.error(result.message)
-            }).always(function() {
-                $btnSubmit.removeAttr("disabled");
-            });
+            update($form.attr("action"), $form.serialize());
         } catch (e) {
             toastr.error(response.message);
         } finally {
             return false;
         }
+    };
+
+    var update = function(url, data) {
+        $btn.button("loading");
+        $.post(url, data, function(result) {
+            if (result.status)
+                toastr.success(result.message)
+            else
+                toastr.error(result.message)
+        }).always(function() {
+            $btn.button("reset");
+        });
     };
 
     $form.submit(submit);

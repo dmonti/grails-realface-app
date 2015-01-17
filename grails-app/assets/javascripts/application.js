6cb00fabfacdc864b2da6efa4a9605466a15cf11
Daniel Monti <dms.monti@gmail.com>
2015-01-17 17:58:40 -0200

    Toastr settings.

--- grails-app/assets/javascripts/application.js
+++ grails-app/assets/javascripts/application.js
@@ -1,16 +1,20 @@
-// This is a manifest file that'll be compiled into application.js.
-//
-// Any JavaScript file within this directory can be referenced here using a relative path.
-//
-// You're free to add application-wide JavaScript to this file, but it's generally better
-// to create separate JavaScript files as needed.
-//
 //= require jquery-2.1.3.min
-//= require handlebars-v2.0.0
-//= require angular.min
 //= require bootstrap.min
-//= require_tree .
+//= require toastr.min
 //= require_self
 
 $(function() {
 });
+
+toastr.options = {
+    "closeButton": false,
+    "progressBar": false,
+    "positionClass": "toast-top-center",
+    "showDuration": "250",
+    "hideDuration": "250",
+    "timeOut": "2500",
+    "showEasing": "swing",
+    "hideEasing": "linear",
+    "showMethod": "fadeIn",
+    "hideMethod": "fadeOut"
+};

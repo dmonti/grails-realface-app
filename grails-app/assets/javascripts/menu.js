6c813d2602a3503b5ce3039223e66766af2ec24f
Daniel Monti <dms.monti@gmail.com>
2015-01-21 17:43:59 -0200

    Code refactor.

--- grails-app/assets/javascripts/menu.js
+++ grails-app/assets/javascripts/menu.js
@@ -1,16 +1,30 @@
+//= require_self
+
 function Menu() {
     var self = this;
+    var locationControllerName;
 
     self.initialize = function() {
-        var urlPathName = window.location.pathname;
+        var locationPathName = window.location.pathname;
+        locationControllerName = getControllerName(locationPathName);
+
         $("#menu").find("li > a").each(function(i, element) {
             var href = $(element).attr("href");
-            if (urlPathName == href) {
-                $(element).parents("li").addClass("active");
+            if (matchLocationPathName(href)) {
+                $(element).parents("li:last").addClass("active");
                 return;
             }
         });
     };
+
+    var matchLocationPathName = function(pathName) {
+        var linkControllerName = getControllerName(pathName);
+        return (linkControllerName == locationControllerName);
+    }
+
+    var getControllerName = function(pathName) {
+        return (pathName.indexOf("/") != -1) ? pathName.split("/")[1] : null;
+    }
 
     self.initialize();
 };

7239a3ec6b059d1b91bbea044b3ab5cb33ea7b47
Daniel Monti <dms.monti@gmail.com>
2015-01-22 20:42:12 -0200

    Error while creating a new Role fixed.

--- grails-app/services/com/realface/RoleService.groovy
+++ grails-app/services/com/realface/RoleService.groovy
@@ -7,16 +7,18 @@ class RoleService
 {
     def updatePersons(Role role, List<String> personIds)
     {
-        Set<PersonRole> oldPersons = role.personRoles;
         Set<PersonRole> newPersons = personIds.collect() {
             Person person = Person.get(it.toLong());
             return new PersonRole([person: person, role: role]);
         }
 
-        Set removed = (oldPersons - newPersons);
-        removed.each() {
-            role.personRoles.remove(it);
-            it.delete();
+        Set<PersonRole> oldPersons = role.personRoles;
+        if (oldPersons) {
+            Set removed = (oldPersons - newPersons);
+            removed.each() {
+                role.personRoles.remove(it);
+                it.delete();
+            }
         }
 
         Set added = (newPersons - oldPersons);

b264717bde0285ae63544e726581f01b139ccd84
Daniel Monti <dms.monti@gmail.com>
2015-01-22 20:53:07 -0200

    Create-drop Heroku datasource.

--- grails-app/conf/DataSource.groovy
+++ grails-app/conf/DataSource.groovy
@@ -31,7 +31,7 @@ environments {
     }
     production {
         dataSource {
-            dbCreate = "update"
+            dbCreate = "create-drop"
             driverClassName = "org.postgresql.Driver"
             dialect = org.hibernate.dialect.PostgreSQLDialect
 

f30be94ce98eb271fbd7d3075cc75196a9e83bfb
Daniel Monti <dms.monti@gmail.com>
2015-01-22 20:37:08 -0200

    Heroku datasource.

--- grails-app/conf/BuildConfig.groovy
+++ grails-app/conf/BuildConfig.groovy
@@ -49,6 +49,7 @@ grails.project.dependency.resolution = {
     dependencies {
         // specify dependencies here under either 'build', 'compile', 'runtime', 'test' or 'provided' scopes e.g.
         runtime "mysql:mysql-connector-java:5.1.34"
+        runtime "org.postgresql:postgresql:9.3-1102-jdbc41" // Heorku DB
         compile "com.google.guava:guava:18.0"
     }
 

7932d719bfeeabbcf1a44792272eb58a81817bf2
Daniel Monti <dms.monti@gmail.com>
2015-01-23 14:31:06 -0200

    Heroku tests.

--- grails-app/conf/BuildConfig.groovy
+++ grails-app/conf/BuildConfig.groovy
@@ -3,22 +3,21 @@ grails.project.class.dir = "target/classes"
 grails.project.test.class.dir = "target/test-classes"
 grails.project.test.reports.dir = "target/test-reports"
 grails.project.work.dir = "target/work"
-grails.project.target.level = 1.6
-grails.project.source.level = 1.6
+grails.project.target.level = 1.8
+grails.project.source.level = 1.8
 //grails.project.war.file = "target/${appName}-${appVersion}.war"
 
 grails.project.fork = [
     // configure settings for compilation JVM, note that if you alter the Groovy version forked compilation is required
-    // compile: [maxMemory: 256, minMemory: 64, debug: false, maxPerm: 256, daemon:true],
-
+    compile: [maxMemory: 256, minMemory: 64, debug: false, maxPerm: 256, daemon:true],
     // configure settings for the test-app JVM, uses the daemon by default
-    test: [maxMemory: 512, minMemory: 64, debug: false, maxPerm: 256, daemon:true],
+    test: [maxMemory: 256, minMemory: 64, debug: false, maxPerm: 256, daemon:true],
     // configure settings for the run-app JVM
-    run: [maxMemory: 512, minMemory: 64, debug: false, maxPerm: 256, forkReserve:false],
+    run: [maxMemory: 256, minMemory: 64, debug: false, maxPerm: 256, forkReserve:false],
     // configure settings for the run-war JVM
-    war: [maxMemory: 512, minMemory: 64, debug: false, maxPerm: 256, forkReserve:false],
+    war: [maxMemory: 256, minMemory: 64, debug: false, maxPerm: 256, forkReserve:false],
     // configure settings for the Console UI JVM
-    console: [maxMemory: 512, minMemory: 64, debug: false, maxPerm: 256]
+    console: [maxMemory: 256, minMemory: 64, debug: false, maxPerm: 256]
 ]
 
 grails.project.dependency.resolver = "maven" // or ivy

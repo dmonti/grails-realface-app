75518d09eb4ede9d89fb7d701b54c4849e55fbdc
Daniel Monti <dms.monti@gmail.com>
2015-06-14 17:59:13 -0300

    Definindo caminho para arquivo de "face points".

--- grails-app/conf/BootStrap.groovy
+++ grails-app/conf/BootStrap.groovy
@@ -1,11 +1,13 @@
+import com.neurotec.plugins.NDataFileManager;
+
 class BootStrap
 {
     def userService;
 
     def init = {  servletContext ->
+        NDataFileManager.getInstance().addFromDirectory("/Volumes/dmonti/Development/Realface/Neurotec_Biometric_5_1_SDK_Trial/Bin/Data", false)
         userService.bootStrap();
     }
 
-    def destroy = {
-    }
+    def destroy = { }
 }

f41eed46ea15a9a719cd054d6a1ff350751b9722
Daniel Monti <dms.monti@gmail.com>
2015-07-05 14:52:06 -0300

    Configurações de banco para o ambiente dev2.

--- grails-app/conf/DataSource.groovy
+++ grails-app/conf/DataSource.groovy
@@ -1,14 +1,14 @@
 dataSource {
     pooled = true
     jmxExport = true
-    driverClassName = "org.h2.Driver"
-    username = "sa"
-    password = ""
+    logSql = false
+
+    driverClassName = "com.mysql.jdbc.Driver"
+    dialect = org.hibernate.dialect.MySQL5InnoDBDialect
 }
 hibernate {
     cache.use_second_level_cache = true
     cache.use_query_cache = false
-//    cache.region.factory_class = 'org.hibernate.cache.SingletonEhCacheRegionFactory' // Hibernate 3
     cache.region.factory_class = 'org.hibernate.cache.ehcache.SingletonEhCacheRegionFactory' // Hibernate 4
     singleSession = true // configure OSIV singleSession mode
     flush.mode = 'manual' // OSIV session flush mode outside of transactional context
@@ -18,15 +18,17 @@ hibernate {
 environments {
     development {
         dataSource {
-            pooled = true
-            logSql = false
-
             dbCreate = "update" // one of 'create', 'create-drop', 'update', 'validate', ''
             url = "jdbc:mysql://localhost:3306/realface?zeroDateTimeBehavior=convertToNull&reconnect=true"
-            driverClassName = "com.mysql.jdbc.Driver"
-            dialect = org.hibernate.dialect.MySQL5InnoDBDialect
-
             username = "root"
+            password = ""
+        }
+    }
+    development2 {
+        dataSource {
+            dbCreate = "update"
+            url = "jdbc:mysql://localhost:3306/realface-grails?zeroDateTimeBehavior=convertToNull&reconnect=true"
+            username = "grails"
             password = ""
         }
     }

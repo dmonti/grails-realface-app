409428b514e7c5e4a2009c3cad48b0e7422103c8
Daniel Monti <dms.monti@gmail.com>
2015-08-18 20:46:52 -0300

    Active menu fixed.

--- grails-app/assets/javascripts/view/menu.js
+++ grails-app/assets/javascripts/view/menu.js
@@ -7,23 +7,33 @@ function Menu() {
     self.initialize = function() {
         var locationPathName = window.location.pathname;
         locationControllerName = getControllerName(locationPathName);
+        locationActionName = getActionName(locationPathName);
+
+        var currentItem;
 
         $("#menu").find("li > a").each(function(i, element) {
             var href = $(element).attr("href");
-            if (matchLocationPathName(href)) {
-                $(element).parents("li:last").addClass("active");
-                return;
+            var linkControllerName = getControllerName(href);
+            var linkActionName = getActionName(href);
+
+            if (locationControllerName == linkControllerName) {
+                currentItem = $(element).parents("li:last");
+                if (locationActionName == linkActionName) {
+                    return false;
+                }
             }
         });
-    };
 
-    var matchLocationPathName = function(pathName) {
-        var linkControllerName = getControllerName(pathName);
-        return (linkControllerName == locationControllerName);
-    }
+        currentItem.addClass("active");
+    };
 
     var getControllerName = function(pathName) {
         return (pathName.indexOf("/") != -1) ? pathName.split("/")[1] : null;
+    }
+
+    var getActionName = function(pathName) {
+        var paths = pathName.split("/")
+        return (pathName.indexOf("/") != -1 && paths.length > 1) ? pathName.split("/")[2] : null;
     }
 
     self.initialize();
